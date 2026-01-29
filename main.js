38436c9fccbb7f99f081e4e595fef1fbda619b8a
Mark <mark.davidovich@gmail.com>
2026-01-29 16:54:48 +0200

    implement tweet button functionality

--- main.js
+++ main.js
@@ -4,5 +4,20 @@ import { Renderer } from "./render.js";
 const tweeter = Tweeter();
 const renderer = Renderer();
 
-// This should render the initial dummy data
-renderer.renderPosts(tweeter.getPosts());
+const loadPosts = () => {
+  renderer.renderPosts(tweeter.getPosts());
+};
+
+loadPosts();
+
+const twitBtn = document.querySelector("#tweet-btn");
+const deletePostBtn = document.querySelectorAll(".delete-tweet-btn");
+const deleteCommentBtn = document.querySelectorAll(".delete-comment-btn");
+const commentBtn = document.querySelectorAll(".comment-btn");
+
+twitBtn.addEventListener("click", () => {
+  const inputElement = document.querySelector("#tweet-input-field");
+  console.log("this shid works?");
+  tweeter.addPost(inputElement.value);
+  loadPosts();
+});
